bf7489e3954a8c6f4a567ad73c1ad7dc90230751
Al-MohtarifCompany <almohtarif.2026@zohomail.com>
2025-05-14 10:00:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def send_telegram_message(bot_token, chat_id, message):
 
         url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 
-        data = {"chat_id": chat_id, "text": text}
+        data = {"chat_id": chat_id, "text": message}
 
         response =  requests.post(url, json=data)
 
